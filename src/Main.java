60ac3387207c36b7d378fa3ea5310a3185ecafda
Merey31 <mereytleuli@gmail.com>
2024-10-22 12:29:58 +0600

    Final final commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Book_Methods test = new Book_Methods();
-        Book[] books;
 
-        books = new Book[] {
-                null,
-                new Book("1984", "George Orwell", 222),
-                new Book("two", "memememe", 424),
-                null,
-                new Book("three", "hehe", 333),
-        };
         }
     }
